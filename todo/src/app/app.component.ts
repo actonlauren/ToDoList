76704c81ae017dcdfbc87e9edc85eb5b312354b7
actonlauren <actonlauren@yahoo.com>
2024-02-08 21:54:36 -0500

    completed foundational coding for assignment

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  task: string = '';
+  completed: boolean = false;
+  duration: number = 0;
+
+  todos: Todo[] = [];
+
+  onSubmit() {
+    const newTodo: Todo = {task: this.task, completed: this.completed, duration: this.duration};    
+    
+    this.todos.push(newTodo);
+  }
+
+
 }
